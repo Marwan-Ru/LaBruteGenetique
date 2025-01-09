e0c58b61e984a6a9fcc4696c71e6f871ab8346fa
marwan <aitaddi.marwan@gmail.com>
2025-01-09 16:54:07 +0100

    Simulation nd player

--- brutegenetique/script/player.gd
+++ brutegenetique/script/player.gd
@@ -1,11 +1,70 @@
+class_name Player
+
 extends Node
 
 var data: PlayerData
 
+var blocking: bool
+var hp : int
+var alive : bool
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	data = PlayerData.new()
+	blocking = false
+	alive = true
+	hp = data.health
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+# Chooses the players action this turn
+# Return 0 or 1 or 2 or 3
+# corresponding to attack or magic or heal or block
+func choose_action() -> int:
+	var r = randf_range(0, 1)
+	
+	if r < data.damage_attack :
+		return 0
+	elif r < data.damage_attack + data.magic_attack:
+		return 1
+	elif r < data.damage_attack + data.magic_attack + data.perform_heal :
+		self.heal()
+		return 2
+	else:
+		blocking = true
+		return 3
+
+func do_action():
+	
+	var action = self.choose_action()
+	
+	match action:
+		0:
+			return action
+		1:
+			return action
+		2:
+			self.heal()
+			return action
+		3:
+			blocking = false
+			return -2
+		_: #Default
+			print("Error in do_action()")
+			return -1000
+
+func take_damage(val):
+	if(!blocking and alive):
+		hp -= val
+		if hp <= 0:
+			hp = 0
+			alive = false
+	blocking = false
+
+func heal():
+	if(alive):
+		hp += data.heal
+		if hp > data.health:
+			hp = data.health
---
+++ brutegenetique/script/simulator.gd
@@ -0,0 +1,71 @@
+extends Node
+
+var playerArray : Array
+var slime : Slime
+var populationSize = 1000
+
+class Slime:
+	var hp : int
+	var attack : int
+	var magic : int
+	var speed : int
+	
+	func _init() -> void:
+		hp = 400
+		attack = 20
+		magic = 20
+		speed = 100
+		
+func _init() -> void:
+	slime = Slime.new()
+	
+	
+func _ready() -> void:
+	#First generation
+	for i in range(populationSize):
+		var player : Player = Player.new()
+		player.data = PlayerData.new()
+		player.data.randomize_parameter()
+		playerArray.append(player)
+
+func simulateFight(player : Player) -> float:
+	while player.alive and slime.hp > 0:
+		if(player.data.speed > slime.speed):
+			var action = player.choose_action()
+			if action == 1:
+				slime.hp -= player.data.damage
+			elif action == 2:
+				slime.hp -= player.data.magic
+		else:
+			var rand = randi_range(0, 1)
+			match rand:
+				0:
+					player.take_damage(slime.attack)
+				1:
+					player.hp -= slime.magic
+		
+		if(player.hp <= 0 or slime.hp <= 0):
+			break
+			
+		if(player.data.speed <= slime.speed):
+			var action = player.choose_action()
+			if action == 1:
+				slime.hp -= player.data.damage
+			elif action == 2:
+				slime.hp -= player.data.magic
+		else:
+			var rand = randi_range(0, 1)
+			match rand:
+				0:
+					player.take_damage(slime.attack)
+				1:
+					player.hp -= slime.magic
+	return 0
+
+#Make everyone fight against the slime and returns an array of scores
+func makeEmFight() -> Array:
+	var scoreArray : Array
+	for i in range(populationSize):
+		scoreArray.append(simulateFight(playerArray[i]))
+		
+	return scoreArray
