f8ad3aaa174a3194d0680e7663fbc573f4d8fb20
Alex <46360108+devalexxx@users.noreply.github.com>
2025-01-09 16:53:11 +0100

    fix: move distribution to utils script

--- brutegenetique/script/player_data.gd
+++ brutegenetique/script/player_data.gd
@@ -1,6 +1,6 @@
 class_name PlayerData
 
-static var points_pool = 100
+var points: int
 
 var damage: int
 var magic : int
@@ -12,38 +12,29 @@ var damage_attack: float
 var magic_attack : float
 var perform_heal : float
 var block_attack : float
-
-func _init() -> void:
-	randomize_parameter()
-
-func distribution(min, pool, size, fn) -> Array:
-	var dist   = range(size).map(func(n): return fn.call(min, pool))
-	var factor = pool as float / dist.reduce(func(acc, n): return acc + n, 0) as float
-	return dist.map(func(n): return n * factor as float) 
-
-func real_distribution(pool, size):
-	return distribution(0, pool, size, randf_range)
-
-func int_distribution(min, pool, size):
-	var dist = distribution(min, pool, size, randi_range).map(func(n): return ceili(n))
-	var diff = pool - dist.reduce(func(acc, n): return acc + n, 0)
-	while true:
-		var idx = randi_range(0, dist.size() - 1)
-		if (dist[idx] + diff >= min):
-			dist[idx] += diff
-			break
-	return dist
-
-func randomize_parameter() -> void:
-	var dist = int_distribution(1, points_pool, 5)
-	damage  = dist[0]
-	magic   = dist[1]
-	heal    = dist[2]
-	speed   = dist[3]
-	health  = dist[4]
 	
-	dist = real_distribution(1.0, 4)
-	damage_attack = dist[0]
-	magic_attack  = dist[1]
-	perform_heal  = dist[2]
-	block_attack  = dist[3]
+static func from_random(pool: int) -> PlayerData:
+	return PlayerData.new(pool, Utils.int_distribution(1, pool, 5), Utils.real_distribution(1.0, 4))
+	
+func _init(points_: int, a: Array, b: Array) -> void:
+	points = points_
+	from_distribution(a, b)
+	
+func from_distribution(a: Array, b: Array) -> void:
+	if a.reduce(func(acc, v): return acc + v, 0) != points:
+		Utils.normalize_distribution(a, points)
+		Utils.fix_int_distribution  (a, points, 1)
+		
+	if not is_equal_approx(b.reduce(func(acc, v): return acc + v, 0), 1.0):
+		Utils.normalize_distribution(a, points)
+		
+	damage = a[0]
+	magic  = a[1]
+	heal   = a[2]
+	speed  = a[3]
+	health = a[4]
+		
+	damage_attack = b[0]
+	magic_attack  = b[1]
+	perform_heal  = b[2]
+	block_attack  = b[3]
---
+++ brutegenetique/script/utils.gd
@@ -0,0 +1,25 @@
+class_name Utils
+
+static func _distribution(min, pool, size: int, fn) -> Array:
+	var dist = range(size).map(func(n): return fn.call(min, pool))
+	return normalize_distribution(dist, pool)
+
+static func real_distribution(pool: float, size: int) -> Array:
+	return _distribution(0, pool, size, randf_range)
+
+static func int_distribution(min: int, pool: int, size: int) -> Array:
+	var dist = _distribution(min, pool, size, randi_range).map(func(n): return ceili(n))
+	return fix_int_distribution(dist, pool, min)
+	
+static func normalize_distribution(dist: Array, pool) -> Array:
+	var factor = pool as float / dist.reduce(func(acc, n): return acc + n, 0) as float
+	return dist.map(func(n): return n * factor as float)
+	
+static func fix_int_distribution(dist: Array, pool, min):
+	var diff = pool - dist.reduce(func(acc, n): return acc + n, 0)
+	while true:
+		var idx = randi_range(0, dist.size() - 1)
+		if (dist[idx] + diff >= min):
+			dist[idx] += diff
+			break
+	return dist
